8842bfe642e7de7f6fe8b937001ba578a2ce044b
ASillyNeko <ASillyNeko@asillyneko.dev>
2026-09-13 13:50:13 -0700

    Format?

--- primedev/scripts/scriptutility.cpp
+++ primedev/scripts/scriptutility.cpp
@@ -28,8 +28,7 @@ ADD_SQFUNC(
 }
 
 // bool function NSIsServerVMValid()
-ADD_SQFUNC(
-	"bool", NSIsServerVMValid, "", "Returns if server vm is valid.", ScriptContext::UI | ScriptContext::CLIENT)
+ADD_SQFUNC("bool", NSIsServerVMValid, "", "Returns if server vm is valid.", ScriptContext::UI | ScriptContext::CLIENT)
 {
 	g_pSquirrel[context]->pushbool(sqvm, g_pSquirrel[ScriptContext::SERVER]->m_pSQVM && g_pSquirrel[ScriptContext::SERVER]->m_pSQVM->sqvm);
 	return SQRESULT_NOTNULL;
